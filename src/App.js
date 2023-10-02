95468c7ade8fbffc362c70f0c0174f642731d37c
Avijit Singh Jhyont <ajhyont@torontomu.ca>
2023-10-02 19:20:31 -0400

    initial commit; So, I have already create an app that allows for user to search for an anime char using their  name.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { CharacterSearch } from './CharacterSearch';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App,App-header">
+      <h1>Which Anime Character Are you?</h1>
+      <CharacterSearch />
     </div>
   );
 }
-
-export default App;
---
+++ src/CharacterSearch.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export const CharacterSearch=()=>{
+
+  const [characterName,setCharacterName] = useState('');
+  const [characterInfo,setCharacterInfo] = useState(null);
+  const variables = { search:characterName.trim().toLowerCase() };
+
+  const fetchData=async()=>{
+    const query=`
+      query($search: String){
+        Character(search: $search){
+          id
+          name{ first last }
+          description
+          image{ large }
+        }
+      }
+    `;
+    try{ 
+      const resp = await axios.post( 'https://graphql.anilist.co',{query,variables,} );
+      setCharacterInfo( resp.data.data.Character );
+    } catch(error){ console.error('Err fetching data:',error); }
+  };
+
+  return(
+    <>
+      <input 
+        type="text"
+        value={characterName}
+        onChange={ (e)=>setCharacterName(e.target.value) }
+        placeholder="Enter Character name"
+      />
+      <button onClick={fetchData}>Fetch</button>
+      { characterInfo && (
+        <div>
+          <h2>{`${characterInfo.name.first} ${characterInfo.name.last}`}</h2>
+          <img src={characterInfo.image.large} alt={characterInfo.name.first} />
+          <p>{characterInfo.description}</p>
+        </div>
+      )}
+    </>
+  );
+
+};
