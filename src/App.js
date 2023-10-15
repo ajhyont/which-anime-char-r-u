32f7eb7ad296da9cd3babe2a41ad002c724a081a
Avijit Singh <ajhyont@torontomu.ca>
2023-10-15 16:39:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { CharacterSearch } from './logic/CharacterSearch';
-import { LandingPage } from './react-related/components/LandingPage';
+import { LandingPage } from './components/LandingPage';
 
 export default function App() {
   return (
