413a011fc8b3080833c6434e04ea0222429b3b05
Avijit Singh <ajhyont@torontomu.ca>
2023-10-20 00:47:25 -0400

    got submit and reset btn working
    Ok so status: selecting options, submitting options, both things work splendidly and when I click submit, the console in chrome does log the list of traits per option, so awesome! I also added a reset btn which we can use 2 clear the global state and local state.
    
    Next up!: I will have the submitted options interact with the charactersearch.js logic and use the options to synethise a set of traits which I'll the use on that js file 2 produce an anime character the user matches to! Awesome! so much 2 look forward 2!

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,19 +1,19 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import questionsData from '../data/list-of-mcqs-and-traits.json';
 import {SelectedOptionsContext} from '../managing-context/SelectedOptionsContext'
 
-export const QuizPage=()=>{  
+export const QuizPage=()=>{    
   const [localSelectedOptions,setLocalSelectedOptions]=useState({});
-  const { setSelectedOptions } = useContext(SelectedOptionsContext);
+  const { selectedOptions, setSelectedOptions, resetSelectedOptions } = useContext(SelectedOptionsContext);
   const handleOptionChange=(e,questionId)=>{
-    setLocalSelectedOptions({
-      ...localSelectedOptions,
-      [questionId]: e.target.value,
-    });
+    setLocalSelectedOptions({ ...localSelectedOptions, [questionId]: e.target.value, });
   };
+  useEffect(()=>{ console.log("Selected options:",selectedOptions); }, 
+    [selectedOptions]
+  );
+
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log("Selectd options:",localSelectedOptions);
     setSelectedOptions(localSelectedOptions);
   };
   return(    
@@ -40,6 +40,7 @@ export const QuizPage=()=>{
       ))}
       <hr />
       <button type="submit">Submit</button>          
+      <button type="button" onClick={()=>{resetSelectedOptions(); setLocalSelectedOptions([]);} }>Reset</button>
       <h1>sample</h1>
     </form>
   );
--- src/managing-context/SelectedOptionsContext.js
+++ src/managing-context/SelectedOptionsContext.js
@@ -3,8 +3,10 @@ import React, {createContext,useState,useContext} from "react";
 export const SelectedOptionsContext = createContext();
 export const SelectedOptionsProvider = ({children})=>{
   const[selectedOptions,setSelectedOptions]=useState([]);
+  const resetSelectedOptions=()=>{ setSelectedOptions({}); };
   return(
-    <SelectedOptionsContext.Provider value={{selectedOptions,setSelectedOptions}}>
+    <SelectedOptionsContext.Provider 
+      value={{ selectedOptions,setSelectedOptions,resetSelectedOptions }}>
       {children}
     </SelectedOptionsContext.Provider>
   );
