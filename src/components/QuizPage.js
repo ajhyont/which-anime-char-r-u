b3b93ff24b100fc9c07356a2f879f4a89bdd6e9b
Avijit Singh <ajhyont@torontomu.ca>
2023-10-22 00:15:22 -0400

    glamming up QuizPage
    I have started beautifying the QuizPage webpage.
    
    NEXT UP: I'll make the mcqs a horizontal list with arrows to navigate thru them. Also, I'll try to get the scroll wheel use for navigation as well.

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,29 +1,30 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import questionsData from '../data/list-of-mcqs-and-traits.json';
-import {SelectedOptionsContext} from '../managing-context/SelectedOptionsContext'
+import { SelectedOptionsContext } from '../managing-context/SelectedOptionsContext'
+import '../styles/global.css';
 import '../styles/QuizPage.css';
 
 export const QuizPage=()=>{    
+
   const [localSelectedOptions,setLocalSelectedOptions]=useState({});
   const { selectedOptions, setSelectedOptions, resetSelectedOptions } = useContext(SelectedOptionsContext);
+  
   const handleOptionChange=(e,questionId)=>{
     setLocalSelectedOptions({ ...localSelectedOptions, [questionId]: e.target.value, });
   };
   useEffect(()=>{ console.log("Selected options:",selectedOptions); }, 
     [selectedOptions]
   );
-
   const handleSubmit=(e)=>{
-    e.preventDefault();
-    setSelectedOptions(localSelectedOptions);
+    e.preventDefault(); setSelectedOptions(localSelectedOptions);
   };
-  return(    
-    <form onSubmit={handleSubmit}>
+  return(
+    <form onSubmit={handleSubmit} className='quiz-form'>
       {questionsData.questions.map((question)=>(
-        <div key={question.id}>
+        <div key={question.id} className='question-container'>
           <h2>{question.id}: {question.que}</h2>                    
           {question.optionsAndTraits.map((option,i)=>(
-            <div key={i}>
+            <div key={i} className='option-container'>
               <input 
                 type='radio'
                 id={`option-${question.id}-${i}`}
@@ -31,8 +32,9 @@ export const QuizPage=()=>{
                 value={option.traits}
                 required
                 onChange={(e)=>handleOptionChange(e,question.id)}
+                className='hidden-radio'
               />
-              <label htmlFor={`option-${question.id}-${i}`}>
+              <label className='label-container' htmlFor={`option-${question.id}-${i}`}>
                 {option.ans}
               </label>
             </div>
@@ -40,8 +42,10 @@ export const QuizPage=()=>{
         </div>
       ))}
       <hr />
-      <button type="submit">Submit</button>          
-      <button type="button" onClick={()=>{resetSelectedOptions(); setLocalSelectedOptions([]);} }>Reset</button>
+      <button type="submit" className='submit-btn'>Submit</button>          
+      <button type="button" className='reset-btn' onClick={()=>{resetSelectedOptions(); setLocalSelectedOptions([]);} }>
+        Reset
+      </button>
       <h1>sample</h1>
     </form>
   );
