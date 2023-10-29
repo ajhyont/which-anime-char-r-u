91db1fe90bc673151b6ef373c8f7bd83d9322234
Avijit Singh <ajhyont@torontomu.ca>
2023-10-29 01:16:45 -0400

    updated project styles and more!
    So, I updated the project styles a little more so that they look more fun and engaging.
    
    NEXT STEPS: I wud now, add 5 morequestions to the mcqs json file and then, submit the proejct to my portfolio. I'll come back 2 it at a later date maybe to add some fun soundless video to it and make the app mobile responsive

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -10,7 +10,7 @@ export const QuizPage=()=>{
 
   const [localSelectedOptions,setLocalSelectedOptions]=useState({});
   const [currentQueIndex,setCurrentQueIndex]=useState(0);
-  const { selectedOptions,setSelectedOptions,resetSelectedOptions,setMatchedCharacter,matchedCharacter } = useContext(SelectedOptionsContext);
+  const { selectedOptions,setSelectedOptions,setMatchedCharacter,matchedCharacter } = useContext(SelectedOptionsContext);
   const navigate = useNavigate();
   const handleOptionChange=(e,questionId)=>{
     setLocalSelectedOptions({ ...localSelectedOptions, [questionId]: e.target.value, });
@@ -30,13 +30,7 @@ export const QuizPage=()=>{
   useEffect( ()=>{
     console.log("Best Match character is:",matchedCharacter);
     }, [matchedCharacter]
-  );  
-  const handleNext=()=>{
-    if(currentQueIndex<questionsData.questions.length-1){ setCurrentQueIndex(currentQueIndex+1); }
-  };  
-  const handlePrev=()=>{
-    if(currentQueIndex>0){ setCurrentQueIndex(currentQueIndex-1); }
-  };
+  );    
   useEffect(()=>{
     document.body.classList.add('quiz-page-body');
     return()=>{ document.body.classList.remove('results-page-body'); };
@@ -66,8 +60,6 @@ export const QuizPage=()=>{
           </div>
         ))}
       </div>
-      <button type='button' onClick={handlePrev} className='arrow-btn left'>←</button>
-      <button type='button' onClick={handleNext} className='arrow-btn right'>→</button>
       <hr />
       <button type="submit" className='submit-btn'>Submit</button>
     </form>
